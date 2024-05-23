0794eead009bdcf9cda29b312081c1d1a1f4c852
Mário Lucas <mariolucasdev@gmail.com>
2024-05-23 16:01:45 -0300

    adding orders table on db

--- .docker/db/init/dump.sql
+++ .docker/db/init/dump.sql
@@ -11,3 +11,15 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT current_timestamp,
     updated_at TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp
 );
+
+CREATE TABLE orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+ALTER TABLE orders ADD FOREIGN KEY (user_id) REFERENCES users (id);
