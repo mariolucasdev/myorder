6f39a317e2efb83e7a70ddf7d8625c8b82c1b63a
Mário Lucas <mariolucasdev@gmail.com>
2024-05-23 22:18:02 -0300

    fix test auth user

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -22,15 +22,25 @@ test('should display login form', function () {
 test('should login user', function () {
     $client = new Client();
 
+    $user = User::create([
+        'first_name' => fake()->firstName(),
+        'last_name' => fake()->lastName(),
+        'document' => fake()->shuffleString('0123456789'),
+        'email' => fake()->email(),
+        'phone_number' => fake()->shuffleString('01234567899'),
+        'birth_date' => fake()->date('Y-m-d'),
+    ]);
+
     $response = $client->post(BASE_URL . '/auth/authenticate', [
         'form_params' => [
-            'email' => 'mariolucasdev@gmail.com',
-            'birth_date' => '1994-06-22'
+            'email' => $user->email,
+            'birth_date' => $user->birth_date
         ]
     ]);
 
     expect($response->getStatusCode())
         ->toBe(200);
+
     expect((string) $response->getBody())
         ->toContain('Usuários Cadastrados');
 });
